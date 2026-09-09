72503c90707fcd47e999dea2373a93a587ff80a3
Helena Botelho de Moraes Costa <helmoraes.4335@gmail.com>
2026-09-09 08:46:33 -0300

    calculadoraC.C_Helena_Ln_valorAbsoluto_GrausRadInverso

--- calculadora C/calculadoraC.C
+++ calculadora C/calculadoraC.C
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <math.h>
+
 #define PI 3.14159265358979323846
 
 float valorAbsoluto(float numero) {
-    return fabs(numero);
-}
-
-int main() {
-    printf("%f\n", valorAbsoluto(-7.5));
-    return 0;
+    if (numero < 0) {
+        return -numero;
+    } else {
+        return numero;
+    }
+    
 }
 
 float logaritmoNatural(float numero) {
@@ -23,6 +24,20 @@ float grausParaRadianos(float graus) {
     return graus * (PI / 180);
 }
 
-float radianosParaGraus(float radianos) {
-    return radianos * (180 / PI);
+int main() {
+    float numero;
+
+    printf("Digite um número para calcular o Valor Absoluto: ");
+    scanf("%f", &numero);
+    printf("Resultado: %f\n\n", valorAbsoluto(numero));
+
+    printf("Digite um número para calcular o Logaritmo Natural: ");
+    scanf("%f", &numero);
+    printf("Resultado: %f\n\n", logaritmoNatural(numero));
+
+    printf("Digite um número para calcular em graus para converter em Radianos: ");
+    scanf("%f", &numero);
+    printf("Resultado: %f\n\n", grausParaRadianos(numero));
+
+    return 0;
 }
